f1e9cca42d8aa2f5ffe2ea78c9e8eb69e2cfc3d2
João Marcelo Coelho Pacheco <jcoelhopacheco@gmail.com>
2023-10-21 09:34:13 -0300

    Começando
    Iniciando o projeto

---
+++ doce-cafeteria/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { useState } from 'react'; 
+
+function App() {
+  const [menu, setMenu]  = useState([])
+  
+  return (
+    <div className="App">
+      <div className='boxtittle'>
+        <p>(Inserir logo Aqui)</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
